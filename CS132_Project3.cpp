453b2d1e5463c572052266ae9c2ea3bdeed12e3f
George Davis <davisgl055@ecc.edu>
2024-04-17 23:18:11 -0400

    Add project files.

---
+++ CS132_Project3.cpp
@@ -0,0 +1,12 @@
+﻿// CS132_Project3.cpp : Defines the entry point for the application.
+//
+
+#include "CS132_Project3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
